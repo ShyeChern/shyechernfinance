359a08c3ad5ca6da5f7aaa7a3c57cdc570b23c3a
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-12-22 22:01:51 +0800

    test

--- src/util/constant.js
+++ src/util/constant.js
@@ -5,5 +5,5 @@ dotenv.config();
 // export const baseUrl = process.env.REACT_APP_BASE_URL;
 export const secret = btoa(`${process.env.REACT_APP_AUTH_NAME}:${process.env.REACT_APP_AUTH_PASS + process.env.REACT_APP_AUTH_SALT}`);
 
-// export const baseUrl = 'http://127.0.0.1:5000/api/';
-export const baseUrl = 'https://shyechernbackend.herokuapp.com:5000/api/';
+export const baseUrl = 'http://127.0.0.1:5000/api/';
+export const baseUrl = 'https://shyechernbackend.herokuapp.com/api/';
